c94be7a64941838a033c122565bf18760ef568f8
IMPrimph <vishnugajulapalli123@gmail.com>
2021-05-09 07:30:02 +0530

    add alerts if todo is less than 4 characters

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Alert } from 'react-native';
 import AddTodo from './components/addTodo';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
@@ -20,11 +20,19 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        { text: text, key: Math.random().toString() }, ...prevTodos
-      ]
-    })
+
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() }, ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('Oops!', 'todos must be over 3 characters long', [
+        { text: 'Understood', onPress: () => console.log('alert closed') }
+      ])
+    }
+
   }
 
   return (
